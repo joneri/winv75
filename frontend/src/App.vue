7313bc906f5b08fb5c1aa9a75430d6be73dbd845
Jonas Eriksson <jonasaeriksson@me.com>
2023-08-15 19:33:08 +0200

    Basic Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <v-app>
+    <NavigationBar />
     <router-view/>
   </v-app>
 </template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar.vue'
+
+export default {
+  components: {
+    NavigationBar,
+  },
+}
+</script>
---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar app>
+        <v-btn to="/" text>
+            Home
+        </v-btn>
+        <v-btn to="/racedays" text>
+            Race Days
+        </v-btn>
+        <!-- Add other navigation buttons/links as needed -->
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar',
+}
+</script>
